312ad18904477ab1a62f2c83c1cac7ffe88f613f
Unomars4 <une.nyide@gmail.com>
2023-04-30 15:39:45 +0200

    update game function

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,13 @@ const CHOICES = ["rock", "paper", "scissors"];
 
 console.log(game());
 
-
 function game(){
     let computerChoice, playerChoice, playerWins = 0, computerWins = 0, roundWinner = 0;
 
     for (let i = 0; i < 5; i++) {
-        playerChoice = prompt("Rock, Paper or Scissors").toLowerCase();
+        do {
+            playerChoice = prompt("Rock, Paper or Scissors").toLowerCase();
+        }while (validInput(playerChoice));
         computerChoice = getComputerChoice();
         roundWinner = playRound(playerChoice, computerChoice);
         switch (roundWinner) {
@@ -61,7 +62,12 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-
+function validInput(word){
+    for (let i = 0, max = CHOICES.length; i < max; i++){
+        if (word == CHOICES[i]) return false;
+    }
+    return true;
+}
 
 function getComputerChoice(){
     return CHOICES[Math.floor(Math.random() * 10 % 3)];    
