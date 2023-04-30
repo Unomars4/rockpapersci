dec50dae2fde0ff313cf05e33168b59d268bfe8f
Unomars4 <une.nyide@gmail.com>
2023-04-30 15:18:00 +0200

    update game function

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,38 @@
 const CHOICES = ["rock", "paper", "scissors"];
 
+console.log(game());
+
 
 function game(){
-    let playerWins = 0, computerWins = 0, roundWinner = 0;
-    let playerChoice = prompt("Rock, Paper or Scissors").toLowerCase;
-    let computerChoice = getComputerChoice();
+    let computerChoice, playerChoice, playerWins = 0, computerWins = 0, roundWinner = 0;
 
     for (let i = 0; i < 5; i++) {
-        console.log(`Round ${i + 1}:`);
+        playerChoice = prompt("Rock, Paper or Scissors").toLowerCase();
+        computerChoice = getComputerChoice();
         roundWinner = playRound(playerChoice, computerChoice);
-        if (roundWinner == 1) {
-            console.log(`You win! `);
-            playerWins += 1;
-        }
-        else if (roundWinner == 2) {
-            console.log(`You lose! `);
-            computerWins += 1;
-        }
-        else{
-            console.log(`It's a tie!`);
-        }
+        switch (roundWinner) {
+            case 1:
+                console.log(`You win! Round:${i+1}`);
+                playerWins++;
+                break;
+            case 2:
+                console.log(`You lose! Round:${i+1}`);
+                computerWins++;
+                break;
+            default:
+                console.log(`It's a tie! Round:${i+1}`);
+                break;    
+        } 
+    }
+
+    if (playerWins > computerWins){
+        return `You won the game. ${playerWins} - ${computerWins}`;
+    }
+    else if (playerWins < computerWins){
+        return `You lost the game. ${playerWins} - ${computerWins}`;
+    }
+    else{
+        return `You both drew. ${playerWins} - ${computerWins}`;
     }
 }
 
