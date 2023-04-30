86246cd765bb9273bd5da21712e4d29c4299999a
Unomars4 <une.nyide@gmail.com>
2023-04-30 14:03:07 +0200

    add playRound function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,34 @@
 const CHOICES = ["rock", "paper", "scissors"];
+let playerChoice = CHOICES[0];
+let computerChoice = getComputerChoice();
 
 
+console.log(playRound(playerChoice, computerChoice));
 
 
-
-
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == CHOICES[0] && computerSelection == CHOICES[1]){
+        return `You lose! Paper beats Rock`;
+    }
+    else if (playerSelection == CHOICES[1] && computerSelection == CHOICES[0]){
+        return `You Win! Paper beats Rock`;
+    }
+    else if (playerSelection == CHOICES[1] && computerSelection == CHOICES[2]){
+        return `You lose! Scissors beats Paper`;
+    }
+    else if (playerSelection == CHOICES[2] && computerSelection == CHOICES[1]){
+        return `You Win! Scissors beats Paper`;
+    }
+    else if (playerSelection == CHOICES[0] && computerSelection == CHOICES[2]){
+        return `You Win! Rock beats Scissors`;
+    }
+    else if (playerSelection == CHOICES[2] && computerSelection == CHOICES[0]){
+        return `You lose! Rock beats Scissors`;
+    }
+    else {
+        return `It's a draw. You both chose ${playerSelection}`;
+    }
+}
 
 
 
